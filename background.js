fbd417d0809ef26460663781bb2dbb339ba13f12
yeemans <arthur.roth.07@gmail.com>
2024-04-18 16:31:09 -0700

    added functionality to create new decks from addCard page

--- background.js
+++ background.js
@@ -1,4 +1,13 @@
-/// options to send highlighted text to front or back of flashcard
+let lastOpenedPopupId = null;
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'popupOpen') {
+    lastOpenedPopupId = sender.tab.id;
+  } else if (message.action === 'popupClose') {
+    lastOpenedPopupId = null;
+  }
+});
+
+// options to send highlighted text to front or back of flashcard
 function createRadioContextMenu() {
     // Create the first radio option
     chrome.contextMenus.create({
@@ -15,38 +24,44 @@ function createRadioContextMenu() {
     });
 }
 
-function showExtensionPopup(frontText, backText, isFront) {
-    chrome.windows.create({
-        url: "newCard.html",
-        type: "popup",
-        width: 400,
-        height: 400
-    }, function(window) {
-        // Delay sending message to ensure popup is ready
+function showExtensionPopup(cardText, isFront) {
+    if (lastOpenedPopupId === null) {
+        chrome.windows.create({
+            url: "newCard.html",
+            type: "popup",
+            width: 400,
+            height: 400
+        }, function(window) {
+            // send message to new tab
+            setTimeout(function() {
+                chrome.tabs.sendMessage(window.tabs[0].id, {
+                    cardText: cardText,
+                    isFront: isFront
+                });
+            }, 100); // 0.1 second delay to load popup.js first
+        });
+    } else {
+        // send message to existing tab
         setTimeout(function() {
-            chrome.tabs.sendMessage(window.tabs[0].id, {
-                frontText: frontText,
-                backText: backText,
+            chrome.tabs.sendMessage(lastOpenedPopupId, {
+                cardText: cardText,
                 isFront: isFront
             });
         }, 100); // 0.1 second delay to load popup.js first
-    });
+    }
 }
 
 // Add event listener to create radio type context menu items when extension is installed
 chrome.runtime.onInstalled.addListener(createRadioContextMenu);
-
-
-
 // Add event listener for clicks on the context menu items
 chrome.contextMenus.onClicked.addListener(function(message, sender, sendResponse) {
     // Check which option was clicked and perform corresponding action
     switch (message.menuItemId) {
         case "front":
-            showExtensionPopup(message.selectionText, "", true);
+            showExtensionPopup(message.selectionText, true);
             break;
         case "back":
-            showExtensionPopup("", message.selectionText, false);
+            showExtensionPopup(message.selectionText, false);
             break;
     }
 });
--- scripts/newCard.js
+++ scripts/newCard.js
@@ -1,10 +1,88 @@
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     let frontInput = document.getElementById("frontInput");
-    if (message.isFront) frontInput.value = message.frontText;    
+    if (message.isFront) frontInput.value = message.cardText;    
 
     let backInput = document.getElementById("backInput");
-    if (!message.isFront) backInput.value = message.backText; 
+    if (!message.isFront) backInput.value = message.cardText; 
 });
 
+chrome.runtime.sendMessage({ action: 'popupOpen' });
+// Handle popup close event
+window.addEventListener('beforeunload', () => {
+  chrome.runtime.sendMessage({ action: 'popupClose' });
+});
 
+function loadDeckSelect() {
+  let deckSelect = document.getElementById("deckSelect");
+  let deckPairs = []
+  for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      deckPairs.push([key, value]);
+  }
+
+  deckPairs.sort(function(a, b) {
+    return a[0] - b[0]; // Compare the first elements of each deckpair
+  });
+
+  for (let keyValuePair of deckPairs)
+    addToDeckSelect(deckSelect, keyValuePair[0], keyValuePair[1]);
+}
+
+function addToDeckSelect(selectElement, deckName, deckList) {
+  deckList = JSON.parse(deckList);
+  if (!deckList.hasOwnProperty("cards")) return ""; // item in local storage that isnt a deck
+
+  var deckOption = new Option(deckName, deckName);
+  // Insert the new option at the beginning of the options array
+  selectElement.appendChild(deckOption);
+}
+
+function addCustomOption() {
+    const selectElement = document.getElementById('deckSelect');
+    const customInput = document.getElementById('customInput');
+  
+    if (selectElement.value === 'custom' && customInput.value.trim() !== '') {
+      const customOption = document.createElement('option');
+      customOption.value = customInput.value;
+      customOption.textContent = customInput.value;
+      selectElement.appendChild(customOption);
+      createNewDeck(customInput.value);
+      customInput.value = '';
+    }
+}
+
+document.getElementById("addNewDeckButton").addEventListener("click", function() {
+    addCustomOption();
+});
+  
+document.getElementById('deckSelect').addEventListener('change', function() {
+    const customInput = document.getElementById('customInput');
+    const newDeckButton = document.getElementById('addNewDeckButton');
+
+    if (this.value === 'custom') {
+      customInput.style.display = 'inline-block';
+      customInput.focus();
+      newDeckButton.style.display = 'inline-block';
+    } else {
+      customInput.style.display = 'none';
+      newDeckButton.style.display = 'none';
+    }
+});
+
+function createNewDeck(deckName) {
+    if (deckAlreadyCreated(deckName)) {
+        document.getElementById("statusMessage").innerText = "Deck already exists.";
+        return; 
+    }
+    
+    localStorage.setItem(deckName, JSON.stringify( {"cards": []} )); // empty array for cards
+    document.getElementById("statusMessage").innerText = "New deck created!";
+}
+
+function deckAlreadyCreated(title) {
+  return (localStorage.getItem(title) !== null);
+}
+  
+loadDeckSelect();
