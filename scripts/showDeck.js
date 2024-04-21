05b219fe64a57b551cc6db789d3c175017b50ade
yeemans <arthur.roth.07@gmail.com>
2024-04-21 11:19:58 -0700

    added shitty slideshow in progress

--- scripts/showDeck.js
+++ scripts/showDeck.js
@@ -44,3 +44,37 @@ function flipCard(flashcard) {
 
 setDeckName();
 
+
+// slideshow javascript
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("flashcard");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    console.log(i);
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+document.querySelector(".prev").addEventListener("click", function() {
+    plusSlides(-1);
+})
+
+document.querySelector(".next").addEventListener("click", function() {
+    plusSlides(1);
+})
+
